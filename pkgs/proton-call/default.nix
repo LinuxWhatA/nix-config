d67868247d0bbd139eb0341d0d1a425d40dcd0cc
LinuxWhatA <linuxwhata@qq.com>
2025-01-05 09:43:54 +0800

    modify proton-call home directory

--- pkgs/proton-call/default.nix
+++ pkgs/proton-call/default.nix
@@ -7,9 +7,9 @@
 
 writeShellScriptBin "proton-call" ''
   [ -f ~/.config/proton.conf ] || cat > ~/.config/proton.conf <<EOF
-    data = "/home/lwa/Documents/"
-    steam = "/home/lwa/.steam/steam/"
-    common = "/home/lwa/.steam/steam/steamapps/common/"
+  data = "$HOME/Documents/"
+  steam = "$HOME/.steam/steam/"
+  common = "$HOME/.steam/steam/steamapps/common/"
   EOF
   ${steam-run}/bin/steam-run ${proton-caller}/bin/proton-call -c ${proton-ge-bin.steamcompattool} $@
 ''
