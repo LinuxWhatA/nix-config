d0a960ef5a8c71157f3d9cbad613edd9138359ea
LinuxWhatA <linuxwhata@qq.com>
2025-01-06 00:18:23 +0800

    update fhs pkg

--- pkgs/fhs/default.nix
+++ pkgs/fhs/default.nix
@@ -2,19 +2,20 @@
   pkgs ? import <nixpkgs> { },
 }:
 
-let
-  base = pkgs.appimageTools.defaultFhsEnvArgs;
-in
-pkgs.buildFHSEnv (
-  base
-  // {
-    name = "fhs";
-    targetPkgs = pkgs: with pkgs; [ icu ] ++ (base.targetPkgs pkgs);
-    profile = ''
-      export IN_NIX_SHELL=impure
-      export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib";
-    '';
-    runScript = "zsh";
-    extraOutputsToInstall = [ "dev" ];
-  }
-)
+pkgs.buildFHSEnv {
+  name = "fhs";
+  includeClosures = true;
+  targetPkgs =
+    pkgs:
+    with pkgs;
+    [
+      icu
+      zenity
+    ]
+    ++ pkgs.appimageTools.defaultFhsEnvArgs.targetPkgs pkgs;
+  profile = ''
+    export IN_NIX_SHELL=impure
+    export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+  '';
+  runScript = pkgs.writeShellScript "fhs" "exec $@";
+}
