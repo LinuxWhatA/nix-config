61e21ae074920d0891f0e75ad5d91a0827bce55c
LinuxWhatA <15975447632@163.com>
2025-01-14 23:35:55 +0800

    fix: proton-caller source

--- overlays/default.nix
+++ overlays/default.nix
@@ -28,5 +28,16 @@ in
           '';
       }
     );
+
+    # proton-caller 换源
+    proton-caller = prev.proton-caller.overrideAttrs (old: {
+      src = old.src.override {
+        owner = "LinuxWhatA";
+        repo = "proton-caller";
+        rev = "7ed1962baf920893368f805dc63c896a2f206176";
+        sha256 = "sha256-gAnsP4AR1NnTCThF29H3qvI+PP459qYX72+OBHX0kV4=";
+      };
+      cargoHash = "sha256-LBXCcFqqscCGgtTzt/gr7Lz0ExT9kAWrXPuPuKzKt0E=";
+    });
   };
 }
