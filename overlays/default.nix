6af0abe6eeb11a4b375d66308d13d8543f527c9e
LinuxWhatA <linuxwhata@qq.com>
2025-01-17 22:18:28 +0800

    style: gnome triple-buffering

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,6 +16,24 @@ in
 
   # Modifies existing packages
   modifications = final: prev: {
+    # Dynamic triple buffering
+    # You might need to disable aliases to make it work
+    # nixpkgs.config.allowAliases = false;
+    # GNOME 46: triple-buffering-v4-46
+    gnome = prev.gnome.overrideScope (
+      gnomeFinal: gnomePrev: {
+        mutter = gnomePrev.mutter.overrideAttrs (old: {
+          src = old.fetchFromGitLab {
+            domain = "gitlab.gnome.org";
+            owner = "vanvugt";
+            repo = "mutter";
+            rev = "triple-buffering-v4-46";
+            hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
+          };
+        });
+      }
+    );
+
     # wechat-uos 添加 Network 分类
     wechat-uos = prev.buildFHSEnv (
       prev.wechat-uos.args
@@ -58,9 +76,10 @@ in
 
     # lutris 添加 winetricks
     lutris = prev.lutris.override {
-      extraPkgs = pkgs: with pkgs; [
-        winetricks
-      ];
+      extraPkgs =
+        pkgs: with pkgs; [
+          winetricks
+        ];
     };
   };
 }
