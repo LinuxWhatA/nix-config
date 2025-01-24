cf9d832dbdbfa5a5b29eda6e63eca11e85ec8250
LinuxWhatA <15975447632@163.com>
2025-01-25 01:36:54 +0800

    feat: git add ignores

--- home/lwa/features/cli/git.nix
+++ home/lwa/features/cli/git.nix
@@ -1,11 +1,15 @@
 { lib, ... }:
 
 {
-  programs = {
-    git = {
-      enable = true;
-      userName = "LinuxWhatA";
-      userEmail = lib.mkDefault "linuxwhata@qq.com";
-    };
+  programs.git = {
+    enable = true;
+    userName = "LinuxWhatA";
+    userEmail = lib.mkDefault "linuxwhata@qq.com";
+
+    lfs.enable = true;
+    ignores = [
+      ".direnv"
+      "result"
+    ];
   };
 }
