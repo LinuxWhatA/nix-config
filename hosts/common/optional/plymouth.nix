f4b633614bbe1b9ff4df7cd98778951c99f22eb7
linuxwhata <5351697+linuxwhata@user.noreply.gitee.com>
2025-02-21 22:39:13 +0800

    plymouth: remove some kernelParams

--- hosts/common/optional/plymouth.nix
+++ hosts/common/optional/plymouth.nix
@@ -4,25 +4,17 @@
 }:
 
 {
-  console = {
-    useXkbConfig = true;
-    earlySetup = false;
-  };
-
   boot = {
     plymouth = {
       enable = true;
       theme = "550w";
       themePackages = [ pkgs.plymouth-550w-theme ];
     };
-    loader.timeout = 0;
+    loader.timeout = 5;
     kernelParams = [
       "quiet"
-      "loglevel=3"
-      "systemd.show_status=auto"
-      "udev.log_level=3"
-      "rd.udev.log_level=3"
-      "vt.global_cursor_default=0"
+      "splash"
+      "plymouth.nolog"
     ];
     consoleLogLevel = 0;
     initrd.verbose = false;
