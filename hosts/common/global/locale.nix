3e620613e358cc6b1cf8b79c9cc92c28a1bf49f4
linuxwhata <5351697+linuxwhata@user.noreply.gitee.com>
2025-04-05 22:36:23 +0800

    feat: support disko-images

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -2,19 +2,8 @@
 
 {
   i18n = {
-    defaultLocale = lib.mkDefault "zh_CN.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "zh_CN.UTF-8";
-      LC_IDENTIFICATION = "zh_CN.UTF-8";
-      LC_MEASUREMENT = "zh_CN.UTF-8";
-      LC_MONETARY = "zh_CN.UTF-8";
-      LC_NAME = "zh_CN.UTF-8";
-      LC_NUMERIC = "zh_CN.UTF-8";
-      LC_PAPER = "zh_CN.UTF-8";
-      LC_TELEPHONE = "zh_CN.UTF-8";
-      LC_TIME = "zh_CN.UTF-8";
-    };
-    supportedLocales = lib.mkDefault [
+    defaultLocale = "zh_CN.UTF-8";
+    supportedLocales = [
       "zh_CN.UTF-8/UTF-8"
     ];
 
