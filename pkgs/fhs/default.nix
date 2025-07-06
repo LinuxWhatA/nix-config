4194b41d9ca415b713c094667ad323d3a272a6a6
linuxwhata <5351697+linuxwhata@user.noreply.gitee.com>
2025-07-06 23:18:39 +0800

    fhs: add multiPkgs

--- pkgs/fhs/default.nix
+++ pkgs/fhs/default.nix
@@ -2,20 +2,17 @@
   pkgs ? import <nixpkgs> { },
 }:
 
+let
+  base = pkgs.appimageTools.defaultFhsEnvArgs;
+in
 pkgs.buildFHSEnv {
   name = "fhs";
   includeClosures = true;
-  targetPkgs =
-    pkgs:
-    with pkgs;
-    [
-      icu
-      zenity
-    ]
-    ++ pkgs.appimageTools.defaultFhsEnvArgs.targetPkgs pkgs;
+  targetPkgs = pkgs: with pkgs; [ icu ] ++ base.targetPkgs pkgs;
+  multiPkgs = base.multiPkgs;
   profile = ''
     export IN_NIX_SHELL=impure
     export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib"
   '';
-  runScript = pkgs.writeShellScript "fhs" "exec $@";
+  runScript = "zsh";
 }
