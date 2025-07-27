034b9a088351f97030bf07ad17af25c32168dd92
LinuxWhatA <5351697+linuxwhata@user.noreply.gitee.com>
2025-07-28 05:44:16 +0800

    heroic: disable update

--- home/lwa/features/games/heroic.nix
+++ home/lwa/features/games/heroic.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = with pkgs; [
@@ -11,6 +11,21 @@
 
   home.file.".config/heroic/tools/proton/Proton-GE" = {
     force = true;
-    source = config.lib.file.mkOutOfStoreSymlink "${pkgs.proton-ge-bin.steamcompattool}";
+    source = "${pkgs.proton-ge-bin.steamcompattool}";
   };
+
+  home.activation.heroicSettings =
+    let
+      config = builtins.toJSON {
+        defaultSettings = {
+          checkForUpdatesOnStartup = false;
+        };
+      };
+    in
+    ''
+      file="$HOME/.config/heroic/config.json"
+      [ -f $file ] || (mkdir -p $(dirname $file) && echo "{}" > $file)
+      json=$(${pkgs.fixjson}/bin/fixjson --minify $file)
+      echo $json '${config}' | ${pkgs.jq}/bin/jq -s add > $file
+    '';
 }
