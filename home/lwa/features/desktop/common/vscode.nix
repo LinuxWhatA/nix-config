7466a3aa1ee1f279ef7aa12486164b5fff0c4c16
LinuxWhatA <5351697+linuxwhata@user.noreply.gitee.com>
2025-07-30 03:36:48 +0800

    vscode: config nixd

--- home/lwa/features/desktop/common/vscode.nix
+++ home/lwa/features/desktop/common/vscode.nix
@@ -1,49 +1,73 @@
-{ pkgs, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 
 {
-  programs.vscode = {
-    enable = true;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      mkhl.direnv
-      ms-python.python
-      jnoortheen.nix-ide
-      ms-python.black-formatter
-      ms-ceintl.vscode-language-pack-zh-hans
-    ];
+  options = {
+    hostname = lib.mkOption {
+      type = lib.types.string;
+    };
   };
+  config = {
+    programs.vscode = {
+      enable = true;
+      profiles.default.extensions = with pkgs.vscode-extensions; [
+        mkhl.direnv
+        ms-python.python
+        jnoortheen.nix-ide
+        ms-python.black-formatter
+        ms-ceintl.vscode-language-pack-zh-hans
+      ];
+    };
 
-  home.activation.vscodeSettings =
-    let
-      argv = builtins.toJSON {
-        "locale" = "zh-cn";
-        "password-store" = "basic";
-        "enable-crash-reporter" = false;
-      };
-      userSettings = builtins.toJSON {
-        "update.mode" = "none";
-        "extensions.autoCheckUpdates" = false;
-        "[nix]" = {
-          "editor.tabSize" = 2;
-          "editor.defaultFormatter" = "jnoortheen.nix-ide";
+    home.activation.vscodeSettings =
+      let
+        argv = builtins.toJSON {
+          "locale" = "zh-cn";
+          "password-store" = "basic";
+          "enable-crash-reporter" = false;
         };
-        "editor.fontLigatures" = true;
-        "window.commandCenter" = false;
-        "window.titleBarStyle" = "custom";
-        "editor.fontFamily" = "'FiraCode Nerd Font Mono'";
-        "terminal.integrated.fontFamily" = "'MesloLGS NF'";
-        "nix.serverPath" = "nixd";
-        "nix.enableLanguageServer" = true;
-      };
-    in
-    ''
-      file="$HOME/.vscode/argv.json"
-      [ -f $file ] || (mkdir -p $(dirname $file) && echo "{}" > $file)
-      json=$(${pkgs.fixjson}/bin/fixjson --minify $file)
-      echo $json '${argv}' | ${pkgs.jq}/bin/jq -s add > $file
+        userSettings = builtins.toJSON {
+          "update.mode" = "none";
+          "extensions.autoCheckUpdates" = false;
+          "[nix]" = {
+            "editor.tabSize" = 2;
+            "editor.defaultFormatter" = "jnoortheen.nix-ide";
+          };
+          "editor.fontLigatures" = true;
+          "window.commandCenter" = false;
+          "window.titleBarStyle" = "custom";
+          "editor.fontFamily" = "'FiraCode Nerd Font Mono'";
+          "terminal.integrated.fontFamily" = "'MesloLGS NF'";
+          "nix.serverPath" = "nixd";
+          "nix.enableLanguageServer" = true;
+          "nix.serverSettings" = {
+            "nixd" = {
+              options = {
+                nixos = {
+                  expr = ''(builtins.getFlake (builtins.toString ./.)).nixosConfigurations."${config.hostname}".options'';
+                };
+                home-manager = {
+                  expr = ''(builtins.getFlake (builtins.toString ./.)).homeConfigurations."${config.home.username}@${config.hostname}".options'';
+                };
+              };
+            };
+          };
+        };
+      in
+      ''
+        file="$HOME/.vscode/argv.json"
+        [ -f $file ] || (mkdir -p $(dirname $file) && echo "{}" > $file)
+        json=$(${pkgs.fixjson}/bin/fixjson --minify $file)
+        echo $json '${argv}' | ${pkgs.jq}/bin/jq -s add > $file
 
-      file="$HOME/.config/Code/User/settings.json"
-      [ -f $file ] || (mkdir -p $(dirname $file) && echo "{}" > $file)
-      json=$(${pkgs.fixjson}/bin/fixjson --minify $file)
-      echo $json '${userSettings}' | ${pkgs.jq}/bin/jq -s add > $file
-    '';
+        file="$HOME/.config/Code/User/settings.json"
+        [ -f $file ] || (mkdir -p $(dirname $file) && echo "{}" > $file)
+        json=$(${pkgs.fixjson}/bin/fixjson --minify $file)
+        echo $json '${userSettings}' | ${pkgs.jq}/bin/jq -s add > $file
+      '';
+  };
 }
